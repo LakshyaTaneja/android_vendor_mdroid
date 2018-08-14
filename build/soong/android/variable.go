33259710d9d7c081b40cc268c90f97ae19baae51
Simon Shields <simon@lineageos.org>
2018-08-14 21:31:03 +0530

    aospa: add soong bits for boardflags
    Change-Id: Iff6c08f1e356ddd9fa0f5ea3308c57df5f5db885

--- build/soong/android/variable.go
+++ build/soong/android/variable.go
@@ -1,57 +1,8 @@
 package android
 type Product_variables struct {
-	Has_legacy_camera_hal1 struct {
-		Cflags []string
-	}
 
-	Uses_media_extensions struct {
-		Cflags []string
-	}
-
-	Needs_text_relocations struct {
-		Cppflags []string
-	}
-
-	Mtk_hardware struct {
-		Cflags []string
-	}
-
-	Cant_reallocate_omx_buffers struct {
-		Cflags []string
-	}
-
-	Use_legacy_rescaling struct {
-		Cflags []string
-	}
-
-	Uses_generic_camera_parameter_library struct {
-		Srcs []string
-	}
-	Uses_qcom_bsp_legacy struct {
-		Cppflags []string
-	}
-	Target_shim_libs struct {
-		Cppflags []string
- 	}
-	Uses_qti_camera_device struct {
-		Cppflags []string
-		Shared_libs []string
-	}
 }
 
 type ProductVariables struct {
-	Has_legacy_camera_hal1  *bool `json:",omitempty"`
-	Uses_media_extensions   *bool `json:",omitempty"`
-	Uses_generic_camera_parameter_library  *bool `json:",omitempty"`
-	Specific_camera_parameter_library  *string `json:",omitempty"`
-	Needs_text_relocations  *bool `json:",omitempty"`
-	QTIAudioPath            *string `json:",omitempty"`
-	QTIDisplayPath          *string `json:",omitempty"`
-	QTIMediaPath            *string `json:",omitempty"`
-	Mtk_hardware            *bool `json:",omitempty"`
-	Cant_reallocate_omx_buffers *bool `json:",omitempty"`
-	Use_legacy_rescaling    *bool `json:",omitempty"`
-	Uses_qcom_bsp_legacy  *bool `json:",omitempty"`
-	Target_shim_libs *string `json:",omitempty"`
-	Uses_qti_camera_device  *bool `json:",omitempty"`
+
 }
--- build/soong/soong_config.mk
+++ build/soong/soong_config.mk
@@ -1,23 +1,9 @@
-mdroid_soong:
-	$(hide) mkdir -p $(dir $@)
-	$(hide) (\
-	echo '{'; \
-	echo '"Mdroid": {'; \
-	echo '    "Has_legacy_camera_hal1": $(if $(filter true,$(TARGET_HAS_LEGACY_CAMERA_HAL1)),true,false),'; \
-	echo '    "Uses_media_extensions": $(if $(filter true,$(TARGET_USES_MEDIA_EXTENSIONS)),true,false),'; \
-	echo '    "Uses_generic_camera_parameter_library": $(if $(TARGET_SPECIFIC_CAMERA_PARAMETER_LIBRARY),false,true),'; \
-	echo '    "Specific_camera_parameter_library": "$(TARGET_SPECIFIC_CAMERA_PARAMETER_LIBRARY)",'; \
-	echo '    "Needs_text_relocations": $(if $(filter true,$(TARGET_NEEDS_PLATFORM_TEXT_RELOCATIONS)),true,false),'; \
-	echo '    "QTIAudioPath":  "$(call project-path-for,qcom-audio)",'; \
-	echo '    "QTIDisplayPath":  "$(call project-path-for,qcom-display)",'; \
-	echo '    "QTIMediaPath":  "$(call project-path-for,qcom-media)",';  \
-	echo '    "Mtk_hardware": $(if $(filter true,$(BOARD_USES_MTK_HARDWARE)),true,false),'; \
-	echo '    "BoardUsesQTIHardware":  $(if $(BOARD_USES_QTI_HARDWARE),true,false),';  \
-	echo '    "Uses_qti_camera_device": $(if $(filter true,$(TARGET_USES_QTI_CAMERA_DEVICE)),true,false),'; \
-	echo '    "Libart_img_base": "$(LIBART_IMG_BASE)",'; \
-	echo '    "Cant_reallocate_omx_buffers":  $(if $(filter omap4,$(TARGET_BOARD_PLATFORM)),true,false),';  \
-	echo '    "Use_legacy_rescaling":  $(if $(strip $(TARGET_OMX_LEGACY_RESCALING)),true,false),';  \
-	echo '    "Uses_qcom_bsp_legacy": $(if $(filter true,$(TARGET_USES_QCOM_BSP_LEGACY)),true,false),'; \
-	echo '    "Target_shim_libs": "$(subst $(space),:,$(TARGET_LD_SHIM_LIBS))"'; \
-	echo '},'; \
-	echo '') > $(SOONG_VARIABLES_TMP)
+_contents := $(_contents)    "Mdroid":{$(newline)
+
+# See build/core/soong_config.mk for the add_json_* functions you can use here.
+
+
+# This causes the build system to strip out the last comma in our nested struct, to keep the JSON valid.
+_contents := $(_contents)__SV_END
+
+_contents := $(_contents)    },$(newline)
