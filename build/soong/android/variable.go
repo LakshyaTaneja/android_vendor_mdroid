32d8d77b216d529e643ff98c245dc023656ed409
Kevin F. Haggerty <haggertk@lineageos.org>
2018-10-15 16:20:05 +0000

    vendor/mdroid: update build/soong

--- build/soong/android/variable.go
+++ build/soong/android/variable.go
@@ -9,10 +9,28 @@ type Product_variables struct {
         Additional_gralloc_10_usage_bits struct {
 		Cppflags []string
         }
+	Has_legacy_camera_hal1 struct {
+		Cflags []string
+	}
+	Uses_generic_camera_parameter_library struct {
+		Srcs []string
+	}
+	Uses_qcom_bsp_legacy struct {
+		Cppflags []string
+	}
+	Uses_qti_camera_device struct {
+		Cppflags []string
+		Shared_libs []string
+	}
 }
 
 type ProductVariables struct {
 	Needs_text_relocations  *bool `json:",omitempty"`
 	Target_shim_libs  *string `json:",omitempty"`
         Additional_gralloc_10_usage_bits  *string `json:",omitempty"`
+        Has_legacy_camera_hal1  *bool `json:",omitempty"`
+	Specific_camera_parameter_library  *string `json:",omitempty"`
+	Uses_generic_camera_parameter_library  *bool `json:",omitempty"`
+	Uses_qcom_bsp_legacy  *bool `json:",omitempty"`
+	Uses_qti_camera_device  *bool `json:",omitempty"`
 }
